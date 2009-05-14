80212e0e6e07501f43eb51d4f19e8564d1fcaeca
nic <nic@06b2ed3a-f15b-0410-800b-a9c9ae62f0a8>
2009-05-14 05:00:26 +0000

    Put a starbase in orbit around the Thraddash homeworld

--- src/sc2code/planets/genthrad.c
+++ src/sc2code/planets/genthrad.c
@@ -19,10 +19,12 @@
 #include "build.h"
 #include "encount.h"
 #include "globdata.h"
+#include "lander.h"
 #include "nameref.h"
 #include "resinst.h"
 #include "setup.h"
 #include "state.h"
+#include "sounds.h"
 #include "planets/genall.h"
 #include "libs/mathlib.h"
 
@@ -102,6 +104,18 @@ GenerateThradd (BYTE control)
 			}
 			pSolarSysState->CurNode = 0;
 			break;
+		case GENERATE_MOONS:
+			pSolarSysState->MoonDesc[0].data_index =
+					(ActivateStarShip (THRADDASH_SHIP, SPHERE_TRACKING)) ?
+					HIERARCHY_STARBASE : DESTROYED_STARBASE;
+			pSolarSysState->MoonDesc[0].radius = MIN_MOON_RADIUS;
+			pSolarSysState->MoonDesc[0].location.x =
+					COSINE (HALF_CIRCLE + OCTANT,
+					pSolarSysState->MoonDesc[0].radius);
+			pSolarSysState->MoonDesc[0].location.y =
+					SINE (HALF_CIRCLE + OCTANT,
+					pSolarSysState->MoonDesc[0].radius);
+			break;
 		case GENERATE_PLANETS:
 		{
 			COUNT angle;
@@ -123,7 +137,7 @@ GenerateThradd (BYTE control)
 			else
 			{
 				pSolarSysState->PlanetDesc[0].data_index = WATER_WORLD;
-				pSolarSysState->PlanetDesc[0].NumPlanets = 0;
+				pSolarSysState->PlanetDesc[0].NumPlanets = 1;
 				pSolarSysState->PlanetDesc[0].radius = EARTH_RADIUS * 98L / 100;
 				angle = ARCTAN (
 						pSolarSysState->PlanetDesc[0].location.x,
@@ -137,6 +151,56 @@ GenerateThradd (BYTE control)
 			break;
 		}
 		case GENERATE_ORBITAL:
+			if ((CurStarDescPtr->Index == THRADD_DEFINED) &&
+					(pSolarSysState->pOrbitalDesc == &pSolarSysState->MoonDesc[0]) &&
+					(pSolarSysState->pOrbitalDesc->pPrevDesc == &pSolarSysState->PlanetDesc[0]))
+			{
+				// If you go to the starbase, move the ship to
+				// the planet instead
+				if (ActivateStarShip (THRADDASH_SHIP, SPHERE_TRACKING))
+				{
+					pSolarSysState->pOrbitalDesc =
+							&pSolarSysState->PlanetDesc[0];
+					GLOBAL (ShipStamp.origin.x) = SIS_SCREEN_WIDTH >> 1;
+					GLOBAL (ShipStamp.origin.y) = SIS_SCREEN_HEIGHT >> 1;
+				}
+				// ...unless the Ilwrath or Kohr-Ah have been through here
+				else
+				{
+					RECT r;
+					LockMutex (GraphicsLock);
+					
+					LoadStdLanderFont (&pSolarSysState->SysInfo.PlanetInfo);
+					pSolarSysState->SysInfo.PlanetInfo.DiscoveryString =
+							CaptureStringTable (
+									LoadStringTable (RUINS_STRTAB));
+
+					ScanContext = CreateContext ();
+					SetContext (ScanContext);
+					SetContextFGFrame (Screen);
+					r.corner.x = (SIS_ORG_X + SIS_SCREEN_WIDTH) - MAP_WIDTH;
+					r.corner.y = (SIS_ORG_Y + SIS_SCREEN_HEIGHT) - MAP_HEIGHT;
+					r.extent.width = MAP_WIDTH;
+					r.extent.height = MAP_HEIGHT;
+					SetContextClipRect (&r);
+
+					DoDiscoveryReport (MenuSounds);
+
+					SetContext (SpaceContext);
+					DestroyContext (ScanContext);
+					ScanContext = 0;
+
+					DestroyStringTable (ReleaseStringTable (
+							pSolarSysState->SysInfo.PlanetInfo.DiscoveryString
+							));
+					pSolarSysState->SysInfo.PlanetInfo.DiscoveryString = 0;
+					FreeLanderFont (&pSolarSysState->SysInfo.PlanetInfo);
+
+					UnlockMutex (GraphicsLock);
+					break; 
+				}
+			}
+
 			if (pSolarSysState->pOrbitalDesc == &pSolarSysState->PlanetDesc[0])
 			{
 				if (ActivateStarShip (THRADDASH_SHIP, SPHERE_TRACKING)
