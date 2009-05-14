6d41813de3f32e11908b1d4192d1dd6f7e7bd0a5
nic <nic@06b2ed3a-f15b-0410-800b-a9c9ae62f0a8>
2009-05-14 05:34:15 +0000

    Put a starbase in orbit around the Ilwrath homeworld

--- src/sc2code/planets/genilw.c
+++ src/sc2code/planets/genilw.c
@@ -19,8 +19,11 @@
 #include "build.h"
 #include "encount.h"
 #include "globdata.h"
+#include "lander.h"
 #include "nameref.h"
 #include "resinst.h"
+#include "setup.h"
+#include "sounds.h"
 #include "state.h"
 #include "planets/genall.h"
 #include "libs/mathlib.h"
@@ -64,6 +67,18 @@ GenerateIlwrath (BYTE control)
 			}
 			pSolarSysState->CurNode = 0;
 			break;
+		case GENERATE_MOONS:
+			pSolarSysState->MoonDesc[0].data_index =
+					(ActivateStarShip (ILWRATH_SHIP, SPHERE_TRACKING)) ?
+					HIERARCHY_STARBASE : DESTROYED_STARBASE;
+			pSolarSysState->MoonDesc[0].radius = MIN_MOON_RADIUS;
+			pSolarSysState->MoonDesc[0].location.x =
+					COSINE (QUADRANT - OCTANT,
+					pSolarSysState->MoonDesc[0].radius);
+			pSolarSysState->MoonDesc[0].location.y =
+					SINE (QUADRANT - OCTANT,
+					pSolarSysState->MoonDesc[0].radius);
+			break;
 		case GENERATE_PLANETS:
 		{
 			COUNT angle;
@@ -79,9 +94,60 @@ GenerateIlwrath (BYTE control)
 					COSINE (angle, pSolarSysState->PlanetDesc[0].radius);
 			pSolarSysState->PlanetDesc[0].location.y =
 					SINE (angle, pSolarSysState->PlanetDesc[0].radius);
+			pSolarSysState->PlanetDesc[0].NumPlanets = 1;
 			break;
 		}
 		case GENERATE_ORBITAL:
+			if ((CurStarDescPtr->Index == ILWRATH_DEFINED) &&
+					(pSolarSysState->pOrbitalDesc == &pSolarSysState->MoonDesc[0]) &&
+					(pSolarSysState->pOrbitalDesc->pPrevDesc == &pSolarSysState->PlanetDesc[0]))
+			{
+				// If you go to the starbase, move the ship to
+				// the planet instead
+				if (ActivateStarShip (ILWRATH_SHIP, SPHERE_TRACKING))
+				{
+					pSolarSysState->pOrbitalDesc =
+							&pSolarSysState->PlanetDesc[0];
+					GLOBAL (ShipStamp.origin.x) = SIS_SCREEN_WIDTH >> 1;
+					GLOBAL (ShipStamp.origin.y) = SIS_SCREEN_HEIGHT >> 1;
+				}
+				// ...unless they're all dead
+				else
+				{
+					RECT r;
+					LockMutex (GraphicsLock);
+					
+					LoadStdLanderFont (&pSolarSysState->SysInfo.PlanetInfo);
+					pSolarSysState->SysInfo.PlanetInfo.DiscoveryString =
+							CaptureStringTable (
+									LoadStringTable (RUINS_STRTAB));
+
+					ScanContext = CreateContext ();
+					SetContext (ScanContext);
+					SetContextFGFrame (Screen);
+					r.corner.x = (SIS_ORG_X + SIS_SCREEN_WIDTH) - MAP_WIDTH;
+					r.corner.y = (SIS_ORG_Y + SIS_SCREEN_HEIGHT) - MAP_HEIGHT;
+					r.extent.width = MAP_WIDTH;
+					r.extent.height = MAP_HEIGHT;
+					SetContextClipRect (&r);
+
+					DoDiscoveryReport (MenuSounds);
+
+					SetContext (SpaceContext);
+					DestroyContext (ScanContext);
+					ScanContext = 0;
+
+					DestroyStringTable (ReleaseStringTable (
+							pSolarSysState->SysInfo.PlanetInfo.DiscoveryString
+							));
+					pSolarSysState->SysInfo.PlanetInfo.DiscoveryString = 0;
+					FreeLanderFont (&pSolarSysState->SysInfo.PlanetInfo);
+
+					UnlockMutex (GraphicsLock);
+					break; 
+				}
+			}
+
 			if (pSolarSysState->pOrbitalDesc == &pSolarSysState->PlanetDesc[0])
 			{
 				if (ActivateStarShip (ILWRATH_SHIP, SPHERE_TRACKING))
