84f22160c57f8afc4c9c741a31c46f5de6dd97fb
nic <nic@06b2ed3a-f15b-0410-800b-a9c9ae62f0a8>
2009-05-17 01:04:19 +0000

    Put a starbase in orbit around the Druuge homeworld

--- src/sc2code/planets/gendru.c
+++ src/sc2code/planets/gendru.c
@@ -22,6 +22,8 @@
 #include "lander.h"
 #include "nameref.h"
 #include "resinst.h"
+#include "setup.h"
+#include "sounds.h"
 #include "state.h"
 #include "planets/genall.h"
 #include "libs/mathlib.h"
@@ -82,6 +84,16 @@ GenerateDruuge (BYTE control)
 			}
 			pSolarSysState->CurNode = 0;
 			break;
+		case GENERATE_MOONS:
+			pSolarSysState->MoonDesc[0].data_index =
+					(ActivateStarShip (DRUUGE_SHIP, SPHERE_TRACKING)) ?
+					DRUUGE_STARBASE : DESTROYED_STARBASE;
+			pSolarSysState->MoonDesc[0].radius = MIN_MOON_RADIUS;
+			pSolarSysState->MoonDesc[0].location.x =
+					COSINE (HALF_CIRCLE, pSolarSysState->MoonDesc[0].radius);
+			pSolarSysState->MoonDesc[0].location.y =
+					SINE (HALF_CIRCLE, pSolarSysState->MoonDesc[0].radius);
+			break;
 		case GENERATE_PLANETS:
 		{
 			COUNT angle;
@@ -96,7 +108,7 @@ GenerateDruuge (BYTE control)
 
 			pSolarSysState->PlanetDesc[0].data_index = DUST_WORLD;
 			pSolarSysState->PlanetDesc[0].radius = EARTH_RADIUS * 50L / 100;
-			pSolarSysState->PlanetDesc[0].NumPlanets = 0;
+			pSolarSysState->PlanetDesc[0].NumPlanets = 1;
 			angle = HALF_CIRCLE - OCTANT;
 			pSolarSysState->PlanetDesc[0].location.x =
 					COSINE (angle, pSolarSysState->PlanetDesc[0].radius);
@@ -109,6 +121,56 @@ GenerateDruuge (BYTE control)
 			break;
 		}
 		case GENERATE_ORBITAL:
+			if ((pSolarSysState->pOrbitalDesc == &pSolarSysState->MoonDesc[0]) &&
+					(pSolarSysState->pOrbitalDesc->pPrevDesc == &pSolarSysState->PlanetDesc[0]))
+			{
+				// If you go to the starbase, move the ship to
+				// the planet instead
+				if (ActivateStarShip (DRUUGE_SHIP, SPHERE_TRACKING))
+				{
+					pSolarSysState->pOrbitalDesc =
+							&pSolarSysState->PlanetDesc[0];
+					GLOBAL (ShipStamp.origin.x) = SIS_SCREEN_WIDTH >> 1;
+					GLOBAL (ShipStamp.origin.y) = SIS_SCREEN_HEIGHT >> 1;
+				}
+				// ...unless the Kohr-Ah have been through here
+				else
+				{
+					RECT r;
+					LockMutex (GraphicsLock);
+					
+					LoadStdLanderFont (&pSolarSysState->SysInfo.PlanetInfo);
+					pSolarSysState->SysInfo.PlanetInfo.DiscoveryString =
+							SetRelStringTableIndex (
+									CaptureStringTable (
+											LoadStringTable (RUINS_STRTAB)), 1);
+
+					ScanContext = CreateContext ();
+					SetContext (ScanContext);
+					SetContextFGFrame (Screen);
+					r.corner.x = (SIS_ORG_X + SIS_SCREEN_WIDTH) - MAP_WIDTH;
+					r.corner.y = (SIS_ORG_Y + SIS_SCREEN_HEIGHT) - MAP_HEIGHT;
+					r.extent.width = MAP_WIDTH;
+					r.extent.height = MAP_HEIGHT;
+					SetContextClipRect (&r);
+
+					DoDiscoveryReport (MenuSounds);
+
+					SetContext (SpaceContext);
+					DestroyContext (ScanContext);
+					ScanContext = 0;
+
+					DestroyStringTable (ReleaseStringTable (
+							pSolarSysState->SysInfo.PlanetInfo.DiscoveryString
+							));
+					pSolarSysState->SysInfo.PlanetInfo.DiscoveryString = 0;
+					FreeLanderFont (&pSolarSysState->SysInfo.PlanetInfo);
+
+					UnlockMutex (GraphicsLock);
+					break; 
+				}
+			}
+
 			if (pSolarSysState->pOrbitalDesc == &pSolarSysState->PlanetDesc[0])
 			{
 				if (ActivateStarShip (DRUUGE_SHIP, SPHERE_TRACKING))
