b2111b40530f7223e59db8076fabee6e65878bd0
Nicolas Simonds <nic@metacloud.com>
2019-08-29 00:22:05 -0700

    Androsynth captain activating the special weapon is consistent with SC1 (Bugzilla Bug #444)

--- sc2/src/uqm/ships/androsyn/androsyn.c
+++ sc2/src/uqm/ships/androsyn/androsyn.c
@@ -406,6 +406,9 @@ androsynth_preprocess (ELEMENT *ElementPtr)
 	else
 	{
 		cur_status_flags &= ~(THRUST | WEAPON | SPECIAL);
+		/* keep the special "on" for the duration of blazer mode, a la SC1 */
+		StarShipPtr->RaceDescPtr->ship_data.captain_control.special =
+				SetRelFrameIndex (StarShipPtr->RaceDescPtr->ship_data.captain_control.special, 2);
 
 					/* protection against vux */
 		if (StarShipPtr->RaceDescPtr->characteristics.turn_wait > BLAZER_TURN_WAIT)
@@ -418,9 +421,13 @@ androsynth_preprocess (ELEMENT *ElementPtr)
 
 		if (StarShipPtr->RaceDescPtr->ship_info.energy_level == 0)
 		{
+			/* turn special off */
+			StarShipPtr->RaceDescPtr->ship_data.captain_control.special =
+					SetRelFrameIndex (StarShipPtr->RaceDescPtr->ship_data.captain_control.special, -2);
 			ZeroVelocityComponents (&ElementPtr->velocity);
 			cur_status_flags &= ~(LEFT | RIGHT
 					| SHIP_AT_MAX_SPEED | SHIP_BEYOND_MAX_SPEED);
+			DrawCaptainsWindow(StarShipPtr);
 
 			StarShipPtr->RaceDescPtr->characteristics.turn_wait =
 					StarShipPtr->RaceDescPtr->characteristics.special_wait;
