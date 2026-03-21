1f165164904526e658aa0bad5a6a1c83e71dfc89
Nicolas Simonds <github@submedia.net>
2026-03-21 15:51:02 +1100

    fix: Ur-Quan fighters will attack/destroy incoming projectiles and asteroids
    This was never presented upstream, as it was just a little experiment
    I tried to see if it impacted the effectiveness of the Dreadnought.
    It is moderately fun, so I left it in.
    
    Short version: Ur-Quan fighters will no longer get splattered by
    stray asteroids, which is cool, but the fact that they are always
    on an attack vector, and individually do low damage with a low
    firing rate, means that any ideas one might get about deploying
    defensive screens of fighters to intercept enemy fire fall apart
    pretty quickly.

--- sc2/src/uqm/ships/urquan/urquan.c
+++ sc2/src/uqm/ships/urquan/urquan.c
@@ -20,6 +20,9 @@
 #include "urquan.h"
 #include "resinst.h"
 
+// to satisfy OBJECT_CLOAKED ()
+#include "uqm/colors.h"
+
 #include "uqm/globdata.h"
 
 #include <stdlib.h>
@@ -206,6 +209,7 @@ fighter_preprocess (ELEMENT *ElementPtr)
 		COUNT orig_facing, facing;
 		SIZE delta_x, delta_y;
 		ELEMENT *eptr;
+		HELEMENT hObject, hNextObject;
 
 		Enroute = TRUE;
 
@@ -247,14 +251,27 @@ fighter_preprocess (ELEMENT *ElementPtr)
 		if (ElementPtr->thrust_wait > 0)
 			--ElementPtr->thrust_wait;
 
-		if (ElementPtr->hTarget)
+		// just for fun: instead of only shooting at the
+		// enemy ship, shoot at anything within range
+		for (hObject = GetTailElement (); hObject; hObject = hNextObject)
 		{
-			LockElement (ElementPtr->hTarget, &eptr);
+			LockElement (hObject, &eptr);
+			hNextObject = GetPredElement (eptr);
 			delta_x = eptr->current.location.x
 					- ElementPtr->current.location.x;
 			delta_y = eptr->current.location.y
 					- ElementPtr->current.location.y;
-			UnlockElement (ElementPtr->hTarget);
+			UnlockElement (hObject);
+
+			// the list of things not to shoot at
+			// includes: yourself, the mothership, other
+			// fighters, non-collidable objects and
+			// cloaked ships
+			if (eptr == ElementPtr || eptr == ElementPtr->pParent ||
+					ElementPtr->pParent == eptr->pParent ||
+					! CollidingElement (eptr) || OBJECT_CLOAKED (eptr))
+				continue;
+
 			delta_x = WRAP_DELTA_X (delta_x);
 			delta_y = WRAP_DELTA_Y (delta_y);
 
@@ -269,29 +286,39 @@ fighter_preprocess (ELEMENT *ElementPtr)
 						ANGLE_TO_FACING (ARCTAN (delta_x, delta_y))
 						);
 				ElementPtr->postprocess_func = fighter_postprocess;
+				break;
 			}
+		}
 
-			if (Enroute)
+		if (ElementPtr->hTarget && Enroute)
+		{
+			LockElement (ElementPtr->hTarget, &eptr);
+			delta_x = eptr->current.location.x
+					- ElementPtr->current.location.x;
+			delta_y = eptr->current.location.y
+					- ElementPtr->current.location.y;
+			UnlockElement (ElementPtr->hTarget);
+			delta_x = WRAP_DELTA_X (delta_x);
+			delta_y = WRAP_DELTA_Y (delta_y);
+
+			facing = GetFrameIndex (eptr->current.image.frame);
+			if (ElementPtr->turn_wait & LEFT)
 			{
-				facing = GetFrameIndex (eptr->current.image.frame);
-				if (ElementPtr->turn_wait & LEFT)
-				{
-					delta_x += COSINE (FACING_TO_ANGLE (facing - 4),
-							DISPLAY_TO_WORLD (30));
-					delta_y += SINE (FACING_TO_ANGLE (facing - 4),
-							DISPLAY_TO_WORLD (30));
-				}
-				else
-				{
-					delta_x += COSINE (FACING_TO_ANGLE (facing + 4),
-							DISPLAY_TO_WORLD (30));
-					delta_y += SINE (FACING_TO_ANGLE (facing + 4),
-							DISPLAY_TO_WORLD (30));
-				}
-				facing = NORMALIZE_FACING (
-						ANGLE_TO_FACING (ARCTAN (delta_x, delta_y))
-						);
+				delta_x += COSINE (FACING_TO_ANGLE (facing - 4),
+						DISPLAY_TO_WORLD (30));
+				delta_y += SINE (FACING_TO_ANGLE (facing - 4),
+						DISPLAY_TO_WORLD (30));
 			}
+			else
+			{
+				delta_x += COSINE (FACING_TO_ANGLE (facing + 4),
+						DISPLAY_TO_WORLD (30));
+				delta_y += SINE (FACING_TO_ANGLE (facing + 4),
+						DISPLAY_TO_WORLD (30));
+			}
+			facing = NORMALIZE_FACING (
+					ANGLE_TO_FACING (ARCTAN (delta_x, delta_y))
+					);
 		}
 		ElementPtr->state_flags |= CHANGING;
 
