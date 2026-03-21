7208fc0104880bec9e2eacdd8578cd5583a278c5
Nicolas Simonds <github@submedia.net>
2026-03-21 15:51:04 +1100

    feat: Put a starbase in orbit around the Ilwrath homeworld

--- sc2/src/uqm/planets/generate/genilw.c
+++ sc2/src/uqm/planets/generate/genilw.c
@@ -28,6 +28,8 @@
 
 
 static bool GenerateIlwrath_generatePlanets (SOLARSYS_STATE *solarSys);
+static bool GenerateIlwrath_generateMoons (SOLARSYS_STATE *solarSys,
+		PLANET_DESC *planet);
 static bool GenerateIlwrath_generateOrbital (SOLARSYS_STATE *solarSys,
 		PLANET_DESC *world);
 static COUNT GenerateIlwrath_generateEnergy (const SOLARSYS_STATE *,
@@ -41,7 +43,7 @@ const GenerateFunctions generateIlwrathFunctions = {
 	/* .reinitNpcs       = */ GenerateDefault_reinitNpcs,
 	/* .uninitNpcs       = */ GenerateDefault_uninitNpcs,
 	/* .generatePlanets  = */ GenerateIlwrath_generatePlanets,
-	/* .generateMoons    = */ GenerateDefault_generateMoons,
+	/* .generateMoons    = */ GenerateIlwrath_generateMoons,
 	/* .generateName     = */ GenerateDefault_generateName,
 	/* .generateOrbital  = */ GenerateIlwrath_generateOrbital,
 	/* .generateMinerals = */ GenerateDefault_generateMinerals,
@@ -61,6 +63,7 @@ GenerateIlwrath_generatePlanets (SOLARSYS_STATE *solarSys)
 	GenerateDefault_generatePlanets (solarSys);
 
 	solarSys->PlanetDesc[0].data_index = PRIMORDIAL_WORLD;
+	solarSys->PlanetDesc[0].NumPlanets = 1;
 	solarSys->PlanetDesc[0].radius = EARTH_RADIUS * 204L / 100;
 	angle = ARCTAN (
 			solarSys->PlanetDesc[0].location.x,
@@ -74,8 +77,35 @@ GenerateIlwrath_generatePlanets (SOLARSYS_STATE *solarSys)
 }
 
 static bool
+GenerateIlwrath_generateMoons (SOLARSYS_STATE *solarSys, PLANET_DESC *planet)
+{
+	if (planet == &solarSys->PlanetDesc[0])
+	{
+		GenerateDefault_generateMoons (solarSys, planet);
+		solarSys->MoonDesc[0].data_index =
+				(StartSphereTracking (ILWRATH_SHIP)) ?
+				HIERARCHY_STARBASE : DESTROYED_STARBASE;
+		solarSys->MoonDesc[0].radius = MIN_MOON_RADIUS;
+		solarSys->MoonDesc[0].location.x =
+				COSINE (OCTANT, solarSys->MoonDesc[0].radius);
+		solarSys->MoonDesc[0].location.y =
+				SINE (OCTANT, solarSys->MoonDesc[0].radius);
+		return true;
+	}
+
+	return GenerateDefault_generateMoons (solarSys, planet);
+}
+
+static bool
 GenerateIlwrath_generateOrbital (SOLARSYS_STATE *solarSys, PLANET_DESC *world)
 {
+	if (matchWorld (solarSys, world, 0, 0))
+	{
+		if (VisitHomeWorldStarBase (StartSphereTracking (ILWRATH_SHIP)))
+			return true;
+		world = &solarSys->PlanetDesc[0];
+	}
+
 	if (matchWorld (solarSys, world, 0, MATCH_PLANET))
 	{
 		if (StartSphereTracking (ILWRATH_SHIP))
